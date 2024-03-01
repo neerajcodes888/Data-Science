cc2aea2a735159af378c8b0507eb07a237264891
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-01 13:07:43 +0000

    Final Commit

--- Machine Learning/Car Price Prediction/app.py
+++ Machine Learning/Car Price Prediction/app.py
@@ -2,24 +2,22 @@ import pandas as pd
 import datetime
 import joblib
 import streamlit as st
-
-
 def main():
     html_tag="""
     <div style = "background-color:grey;padding:15px">
     <h2 style="color:red;text-align:center;">Car Selling Price Prediction World</h2>
     <ul>
-     <li style="color:red;">
-     <h5 style="color:blue;text-align:centre;">Welcome to Car Prediction System Made by Machine Learning </h5>
+        <li style="color:red;">
+        <h5 style="color:blue;text-align:centre;">Welcome to Car Prediction System Made by Machine Learning </h5>
         </li>
-            <li style="color:red;"> 
+        <li style="color:red;"> 
         <h5 style="color:blue;text-align:centre;">     Predict Your Car selling Price at best price</h5>
         </li>
         <li style="color:red;">
         <h5 style="color:blue;text-align:centre;">Fill the details Carefully!!!</h5></li>
-    </ul>
+    </ul>   
     
-        <h5 style="color:black;text-align:left;">Incase of Clarifiction or doubts Contact me:-> 
+    <h5 style="color:black;text-align:left;">Incase of Clarifiction or doubts Contact me:-> 
     <ul>
       <li style="color:yellow;">
       <a href="https://www.linkedin.com/in/neeraj-kumar-9a75811a2" style="color:#36AE7C;">Linkdin Profile</a>
@@ -30,7 +28,6 @@ def main():
    </ul>
     </h5> 
       </div>
-
     """
     model=joblib.load('Car_price_Prediction_model')
     
@@ -78,6 +75,7 @@ def main():
         #  st.warning("OOPS....Unable to predict selling Price")
     except:
         st.warning("Some error occured! Please Try again!!!")
+
 
 
 if __name__ == '__main__':
