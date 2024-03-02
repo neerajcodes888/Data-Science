e3e979f7eb45d3af40c44e3312588db4eb6a5397
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-02 20:01:29 +0000

    ccccc

--- Machine Learning/Loan Approval Prediction/app.py
+++ Machine Learning/Loan Approval Prediction/app.py
@@ -14,7 +14,25 @@ def home():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    pass
+        if request.method ==  'POST':
+            gender = request.form['gender']
+            married = request.form['married']
+            dependents = request.form['dependents']
+            education = request.form['education']
+            employed = request.form['employed']
+            credit = float(request.form['credit'])
+            area = request.form['area']
+            ApplicantIncome = float(request.form['ApplicantIncome'])
+            CoapplicantIncome = float(request.form['CoapplicantIncome'])
+            LoanAmount = float(request.form['LoanAmount'])
+            Loan_Amount_Term = float(request.form['Loan_Amount_Term'])
+            
+        # gender
+        if (gender == "Male"):
+            male=1
+        else:
+            male=0
+            
 
 
 if __name__ == "__main__":
