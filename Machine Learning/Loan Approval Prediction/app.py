8a703836624edb80177165690c2bceb4d71a3459
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-02 19:59:44 +0000

    cccc

--- Machine Learning/Loan Approval Prediction/app.py
+++ Machine Learning/Loan Approval Prediction/app.py
@@ -3,10 +3,18 @@ import joblib
 import numpy as np
 
 app = Flask(__name__)
+
+model  = joblib.load('Prediction Model')
+
 @app.route('/')
 
 def home():
     return render_template("index.html")
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    pass
 
 
 if __name__ == "__main__":
