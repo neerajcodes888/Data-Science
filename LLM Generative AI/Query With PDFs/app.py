5589edd9b7102a64f34af93508e5fc93c035fcdc
neeraj <neerajmail888@gmail.com>
2024-03-16 15:26:06 +0530

    cccc

--- LLM Generative AI/Query With PDFs/app.py
+++ LLM Generative AI/Query With PDFs/app.py
@@ -55,3 +55,20 @@ def get_conversational_chain():
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
     return chain
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Response:\n\n\n\n", response["output_text"])
