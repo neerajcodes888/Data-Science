adfce6564ee7e32bb0466e59d22d8c46b00ec481
neeraj <neerajmail888@gmail.com>
2024-03-16 15:24:37 +0530

    cccc

--- LLM Generative AI/Query With PDFs/app.py
+++ LLM Generative AI/Query With PDFs/app.py
@@ -14,3 +14,12 @@ from dotenv import load_dotenv
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
