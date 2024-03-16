d502e992c3830885b5aee26e0f3acef5a81ce456
neeraj <neerajmail888@gmail.com>
2024-03-16 15:25:50 +0530

    cccc

--- LLM Generative AI/Query With PDFs/app.py
+++ LLM Generative AI/Query With PDFs/app.py
@@ -37,4 +37,21 @@ def get_vector_store(text_chunks):
     vector_store.save_local("faiss_index")
     
     
-    
+def get_conversational_chain():
+
+    prompt_template1 = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template1, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
