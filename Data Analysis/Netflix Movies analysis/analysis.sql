4e9eeaeebb53bca234e1f07263b89fa6dfa781b3
Neeraj Kumar <neerajmail888@gmail.com>
2025-05-07 14:37:09 +0000

    COMMIT:

--- Data Analysis/Netflix Movies analysis/analysis.sql
+++ Data Analysis/Netflix Movies analysis/analysis.sql
@@ -1,1 +1,9 @@
 CREATE TABLE new.netflix_titles (show_id text, type text, title text, director text, cast text, country text, date_added text, release_year int, rating text, duration text, listed_in text, description text)	OK	0.000 sec
+
+
+
+--   1. Count the number of Movies vs TV Shows -----
+
+
+
+select count(*) as count, type from netflix_titles where type in('Movie','TV Show') group by type;
