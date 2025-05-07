0ea41245f7345168d10d1a27f6092a2e34449048
Neeraj Kumar <neerajmail888@gmail.com>
2025-05-07 14:18:03 +0000

    commit

---
+++ Data Analysis/Netflix Movies analysis/analysis.sql
@@ -0,0 +1,1 @@
+CREATE TABLE new.netflix_titles (show_id text, type text, title text, director text, cast text, country text, date_added text, release_year int, rating text, duration text, listed_in text, description text)	OK	0.000 sec
