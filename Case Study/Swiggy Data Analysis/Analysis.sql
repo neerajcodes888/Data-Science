d2b97c2e95cde3b1727b70a9c99efc1f4b202df5
Neeraj Kumar <neerajmail888@gmail.com>
2025-05-28 22:15:20 +0530

    Update Analysis.sql

--- Case Study/Swiggy Data Analysis/Analysis.sql
+++ Case Study/Swiggy Data Analysis/Analysis.sql
@@ -3,3 +3,6 @@
 -- 1. Find customers who have never ordered  
 
 select * from users where user_id not in(select user_id from orders);
+
+-- 2. Average Price/dish
+select round(avg(amount),2) from orders;
