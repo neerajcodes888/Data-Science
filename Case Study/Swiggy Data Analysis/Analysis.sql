0fb316d34896c72be06da954df86f920d39a76b1
Neeraj Kumar <neerajmail888@gmail.com>
2025-05-29 18:49:10 +0530

    Update Analysis.sql

--- Case Study/Swiggy Data Analysis/Analysis.sql
+++ Case Study/Swiggy Data Analysis/Analysis.sql
@@ -6,3 +6,22 @@ select * from users where user_id not in(select user_id from orders);
 
 -- 2. Average Price/dish
 select round(avg(amount),2) from orders;
+
+
+-- 3. Find top restaurant in terms of number of orders for a given month
+
+
+-- 4. Restaurants with monthly sales > x for
+
+
+-- 5. Show all orders with order details for a particular customer in a particular date range
+
+
+-- 6. Find restaurants with max repeated customers
+
+
+-- 7. Month over month revenue growth of Swiggy
+
+
+-- 8. Customer → favorite food
+
